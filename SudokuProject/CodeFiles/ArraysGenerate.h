e256c83aea81e06e2b5dc1637e775373aacbdb04
jiuweilinghu <1354895960@qq.com>
2017-09-10 01:37:53 +0800

    数独作业2.0:1.修改代码规范；2.改进文件输出板块，提高运行速度；3.修复生成数独重复的bug

---
+++ SudokuProject/CodeFiles/ArraysGenerate.h
@@ -0,0 +1,15 @@
+#pragma once
+#include<string>
+#include <algorithm>
+
+using namespace std;
+
+class ArraysGenerate
+{
+public:
+	int* toGenerate();         //生成一个随机数组
+private:
+	string str="123456789";   //生成随机数组的原始字符串
+	int array[10];            //存放所生成的随机数组
+};
+
