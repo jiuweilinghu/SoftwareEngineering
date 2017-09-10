8dc3535d22bd8ab9a4b0db5a0659bab5a6b53a7b
jiuweilinghu <1354895960@qq.com>
2017-09-10 22:00:35 +0800

    数独作业3.0: 1.将Debug版的可执行文件改为Release版的；2.优化输出函数，提升运行效率；3.修复生成重复数独的bug；4.修改部分代码规范；5.修改文件目录组织形式

---
+++ SudokuProject/ArraysGenerate.cpp
@@ -0,0 +1,27 @@
+/*
+用以生成一个由数字1~9组成的数组
+*/
+#include "ArraysGenerate.h"
+
+int* ArraysGenerate::toGenerate() {
+
+	while (next_permutation(str.begin(), str.end()) && str[4] == '5')
+	{
+		/**
+		next_permutation(str.begin(), str.end())
+		函数可以将字符串进行全排列，
+		使用时要包含algorithm头文件。
+		另外，要求棋盘左上角为座号后两位相加对9取模再加一，
+		我的尾号为13，所以左上角恒为5。
+		*/
+
+		for (int i = 1; i <= 9; i++)
+		{
+			array[i] = str[i - 1] - 48;      //字符转为数字
+		}
+		break;
+	}
+
+	return array;
+}
+
